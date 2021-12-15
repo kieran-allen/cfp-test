f755a144da69c45a1b0d1095d0da87f91656030e
Kieran Allen <kieran.allen@ovoenergy.com>
2021-12-15 18:17:36 +0000

    add function

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import clsx from 'clsx'
+import { useEffect } from 'react'
 
 export function App() {
+  useEffect(() => {
+    fetch('/api/hello')
+  }, [])
   return (
     <h1 className={clsx('bg-gray-100', 'm-8', 'rounded', 'p-8', 'shadow')}>
       This is DEV build 2
