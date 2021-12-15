43ef5a7234c9d6c0c430fa426168f76b739bd936
Kieran Allen <kieran.allen@ovoenergy.com>
2021-12-15 17:59:29 +0000

    this is dev

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import clsx from 'clsx'
 export function App() {
   return (
     <h1 className={clsx('bg-gray-100', 'm-8', 'rounded', 'p-8', 'shadow')}>
-      Commit number 2
+      This is DEV
     </h1>
   )
 }
