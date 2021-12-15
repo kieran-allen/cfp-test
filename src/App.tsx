6eb299bea02cb0908e817c6b23bf9cc5fc76ab79
Kieran Allen <kieran.allen@ovoenergy.com>
2021-12-15 18:09:00 +0000

    second build on dev

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import clsx from 'clsx'
 export function App() {
   return (
     <h1 className={clsx('bg-gray-100', 'm-8', 'rounded', 'p-8', 'shadow')}>
-      This is DEV
+      This is DEV build 2
     </h1>
   )
 }
